69eb80d82c174c960c191e6d84119badffb0f033
Liron_Hatabi <76784947+LironHatabi@users.noreply.github.com>
2024-06-03 12:53:02 +0000

    chnage await

--- my-app.Service/Controllers/HelloController.cs
+++ my-app.Service/Controllers/HelloController.cs
@@ -18,7 +18,15 @@ namespace my_app.Service.Controllers
         // [Route("hello")]
         public IActionResult GetTest()
         {
-
+            var value = System.Environment.GetEnvironmentVariable("version");
+            return Ok(value);
+        }
+        [HttpGet("liran")]
+        // [Route("hello")]
+        public IActionResult GetLiran()
+        {
+            // await client.GetAsync("http://user9-dotnet-app/user9-application:8080/Hello/hello")
+            await client.GetAsync("http://user9-dotnet-app.user9-application:8080/Hello/hello");
             var value = System.Environment.GetEnvironmentVariable("version");
             return Ok(value);
         }
